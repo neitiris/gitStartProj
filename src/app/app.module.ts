8754003e413fe43b30de7e79d496b36f24666048
neitiris <veliantos@gmail.com>
2018-06-29 14:02:49 +0300

    creating pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RoutesComponent } from '../routes/routes.component';
+import { ServicesComponent } from './services/services.component';
+import { SharedComponent } from './shared/shared.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutesComponent
+    RoutesComponent,
+    ServicesComponent,
+    SharedComponent,
+    AboutComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
